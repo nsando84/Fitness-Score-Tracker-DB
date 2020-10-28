29de3a5b9ac63d273c157ddd0a7ae611fd9a8b79
nicksandoval <sand8val@gmail.com>
2020-10-28 00:04:46 -0700

    converting text to lowercase to help fix errors

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@
 // add evaluation button // 
 
 $('#add-workout-button').on('click', event => {
-    const airmanName = $('#airman-name').val()
+    const airmanName = $('#airman-name').val().toLowerCase()
     const cardioTime = $('#cardio-time-1').val() + $('#cardio-time-2').val()
     const bodyComposition = $('#body-composition').val()
     const pushUps = $('#push-ups').val()
